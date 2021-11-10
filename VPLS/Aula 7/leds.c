d31de7c678f63091f30efdd755c218ccf9c8f67e
JPLS2000 <joaopedro.jplsouza@gmail.com>
2021-11-10 13:56:28 -0300

    push

--- VPLS/Aula 7/leds.c
+++ VPLS/Aula 7/leds.c
@@ -2,18 +2,41 @@
 #include <string.h>
 int main()
 {
-    int n[1000], leds = 0, count[30];
-    int v = 0, result = 0;
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++)
+    int n[1000], leds[1000],nlinha=0 ;
+    int v [100];
+    scanf("%d", &nlinha);
+    for (int i = 0; i < (nlinha-1); i++)
     {
-        scanf("%d", &v);
+        scanf("%d", v);
         n[i] = v;
-        result = v;
-        while (result != 0)
+        for (int z = 0; z< strlen(v); z++)
         {
+            if (v[z]==0||v[z]==9||v[z]==6)
+            {
+                leds[i]=leds[i]+6;
+            }
+            else if (v[z]==2||v[z]==3||v[z]==5)
+            {
+                leds[i]=leds[i]+5;
+            }else if (v[z]==1)
+            {
+                leds[i]=leds[i]+2;
+            }else if (v[z]==7)
+            {
+                leds[i]=leds[i]+3;
+            }else if (v[z]==4)
+            {
+                leds[i]=leds[i]+4;  
+            }else
+            {
+                leds[i]=leds[i]+7;
+            }
+
         }
+        
+        
     }
+    printf("%d",leds);
 
     return 0;
 }
