e23bb6f29a677cdc70dd6924ba8ea5c99da8becd
JPLS2000 <joaopedro.jplsouza@gmail.com>
2021-11-18 08:45:51 -0300

    push

--- VPLS/Aula 7/matrizQuadarada.c
+++ VPLS/Aula 7/matrizQuadarada.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
     int x=0,testes[100],f=0;
-    int m[100],m[100][100];
+    int m[100][100];
     while (x!=0)
     {
         scanf("%d",&x);
@@ -10,7 +10,12 @@ int main(){
     }
     for (int i = 0; i < f; i++)
     {
-        m[testes[i]]
+        m[testes[i]][testes[i]];
+        if (testes[i]%2==0)
+        {
+            /* code */
+        }
+        
     }
     
     
