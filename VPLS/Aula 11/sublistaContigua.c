413f00a0ac924573edcc2893bdabd96fee924247
JPLS2000 <joaopedro.jplsouza@gmail.com>
2021-12-12 21:22:25 -0300

    push

--- VPLS/Aula 11/sublistaContigua.c
+++ VPLS/Aula 11/sublistaContigua.c
@@ -1,13 +1,81 @@
 #include <stdio.h>
+#define MAX 1000
+
+typedef struct
+{
+    int vetor[MAX];
+    int n;
+
+} ARRAY;
 int sublista(int v[]);
 int main(){
-    int vetor[9];
-    for (int i = 0; i < 9; i++)
-    {
-        scanf("%d",vetor[i]);
-    }
-
+   ARRAY array;
+	array.n = 0;
     return 0;
 }
+int elemento(ARRAY *array, int posicao)
+{
+	return array->vetor[posicao];
+}
+
+int tamanho(ARRAY *array)
+{
+	return array->n;
+}
+
+void insere(ARRAY *array, int elemento)
+{
+    array->vetor[tamanho(array)] = elemento;
+    array->n += 1;
+}
+
+void remove_posicao(ARRAY *array, int posicao)
+{
+	if(tamanho(array) == 0)
+	{
+		return;
+	}
+	else if(tamanho(array) == 1)
+	{
+		array->n -= 1;
+		return;
+	}
+	else
+	{
+		array->n -= 1;
+		array->vetor[posicao] = array->vetor[tamanho(array)];
+	}
+}
+
+void remove_elemento(ARRAY *array, int valor)
+{
+	int i;
+
+	for(i = 0; i < tamanho(array); i++)
+	{
+		if(elemento(array, i) == valor)
+		{
+			remove_posicao(array, i);
+		}
+	}
+}
+
+void imprime(ARRAY *array)
+{
+	int i;
+
+	printf("Vetor: ");
+
+	for(i = 0; i < tamanho(array); i++)
+	{
+		if(i != tamanho(array) - 1)
+			printf("%d, ", elemento(array, i));
+		else
+			printf("%d", elemento(array, i));
+	}
+
+	printf("\n");
+}
+
 int sublista(int v[]){
 }
--- VPLS/Aula 8/conversaoBase.c
+++ VPLS/Aula 8/conversaoBase.c
@@ -1,100 +1,160 @@
 #include <stdio.h>
-int casos(int num);
-int transType(char num[],char tipo[],int tam);
-char zerando(char v[]);
-int limitador(char v[]);
-int main(){
-    int x=0;
-    scanf("%d",&x);
-    casos(&x);
+#include <math.h>
+int decBin(int n);
+int decHex(int n);
+int binHex(int n);
+int binDec(int n);
+int main()
+{
+    int n,z,j;
+    long int vetor[30];
+    char number[30], base[30];
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++)
+    {
+        vetor[i] = 0;
+        scanf("%s", number);
+        scanf("%s", &base[i]);
+        if(base[i] == 'h')
+        {
+            for( z = 0; number[z] != '\0'; z++){}
+            for( j = 0; number[j] != '\0'; j++)
+            {
+                if(number[j] >= 'A' && number[j] <= 'Z')
+                {
+                    vetor[i] += (number[j] - 'A' + 10) * pow(16, z - 1);
+                    z--;
+                }
+                else if(number[j] >= 'a' && number[j] <= 'z')
+                {
+                    vetor[i] += (number[j] - 'a' + 10) * pow(16, z - 1);
+                    z--;
+                }
+                else
+                {
+                    vetor[i] += (number[j] - '0') * pow(16, z - 1);
+                    z--;
+                }
+            }
+        }
+        else
+        {
+            for(z = 0; number[z] != '\0'; z++){}
+            for( j = 0; number[j] != '\0'; j++)
+            {
+                vetor[i] += (number[j] - '0') * pow(10, z - 1);
+                z--;
+            }
+        }
+    }
+    for(int i = 0; i < n; i++)
+    {
+        switch(base[i])
+        {
+            case 'b':
+                printf("Case %d\n", i + 1);
+                binDec(vetor[i]);
+                binHex(vetor[i]);
+                break;
+                
+            case 'd':
+                printf("Case %d\n", i + 1);
+                decHex(vetor[i]);
+                decBin(vetor[i]);
+                break;
+                
+            case 'h':
+                printf("Case %d\n", i + 1);
+                printf("%ld dec\n", vetor[i]);
+                decBin(vetor[i]);
+                break;
+        }
+    }
     return 0;
 }
-int casos(int num){
-    char tipo[3], numero[100000];
-    int tam=0;
-    zerador(numero);
-    for (int i = 0; i < num; i++)
+int decBin(int n){
+    int count = 0;
+    int vetor[30];
+    while(n  > 1)
     {
-        scanf("%s %s",numero,tipo);
-        tam =limitador(numero);
-        transType(numero, tipo, tam);
+        vetor[count] = n%2;
+        n /= 2;
+        count++;
     }
-}
-int transType(char num[], char tipo[],int tam){
-    int numero[tam],numeroInt=0,soma=0;
-    if (tipo[0]=='d')
+    vetor[count] = n;
+    for(int i= count; i >= 0; i--)
     {
-        for (int i = 0; i < tam; i++)
+        printf("%d", vetor[i]);
+        if(i==0)
         {
-            switch (num[i])
-            {
-            case '0':
-                numero[i]=0;
-                break;
-            case '1':
-                numero[i]=1;
-                break;
-            case '2':
-                numero[i]=2;
-                break;
-            case '3':
-                numero[i]=3;
-                break;
-            case '4':
-                numero[i]=4;
-                break;
-            case '5':
-                numero[i]=5;
-                break;
-            case '6':
-                numero[i]=6;
-                break;
-            case '7':
-                numero[i]=7;
-                break;
-            case '8':
-                numero[i]=8;
-                break;
-            case '9':
-                numero[i]=9;
-                break; 
-            }
-        }
-        
-    }else if (tipo[0]=='b')
-    {
-        for (int i = 0; i < tam; i++)
-        {
-            switch (num[i])
-            {
-            case '0':
-                numero[i]=0;
-                break;
-            case '1':
-                numero[i]=1;
-                break;
-            }
-            
-        }
-        
-    }
-    
-}
-char zerando(char v[]){
-for (int i = 0; i < 50 ; i++)
-    {
-        v[i]=0;
-    }
-}
-int limitador(char v[]){
-int tam=0;
-    for (int i = 0; i < 100000 ; i++)
-    {
-        if (v[i]==0)
-        {
-            tam=i;
-            break;
+            printf(" bin");
         }
     }
-    return tam;
+    printf("\n");
+}
+
+int decHex(int n)
+{
+    int count = 0;
+    char vetor[30];
+    while(n > 15)
+    {
+        if(n%16 > 9)
+        {
+            vetor[count] = (n%16) % 10 + 'a';  
+            n = n/16;
+            count++;
+        }
+        else
+        {
+            vetor[count] = n%16 + '0';
+            n = n/16;
+            count++;
+        }
+    }
+    if(n > 9)
+    {
+        vetor[count] = (n % 10) + 'a';
+        count++;
+    }
+    else
+    {
+        vetor[count] = n + '0';
+        count++;
+    }
+    for(int i = count - 1; i >= 0; i--)
+    {
+        printf("%c", vetor[i]);
+        if(i==0)
+        {
+            printf(" hex");
+        }
+    }
+    printf("\n");
+}
+
+int binHex(int n)
+{
+    int decimal = 0, i = 0;
+    while(n > 1)
+    {
+        decimal += (n%10) * pow(2, i);
+        n = n/10;
+        i++;
+    }
+    decimal += n * pow(2, i);
+    decHex(decimal);
+}
+
+int binDec(int n)
+{
+    int decimal = 0, i = 0;
+    while(n > 1)
+    {
+        decimal += (n%10) * pow(2, i);
+        n = n/10;
+        i++;
+    }
+    decimal += n * pow(2, i);
+    printf("%d dec\n", decimal);
 }
